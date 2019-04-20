bf6505e2d638f19ef24a985159ce5d48225c3101
MarcosMoura <marcos_moura@live.com>
2019-04-20 13:08:11 -0300

    Update User

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,19 +1,17 @@
 'use strict'
 
-const Database = use('Database')
+// const Database = use('Database')
 const User = use('App/Models/User')
 class UserController {
   async store ({ request }) {
     const data = request.only(['name', 'surname', 'email', 'password'])
-    const addresses = request.input('addresses')
+    // const addresses = request.input('addresses')
+    // const trx = await Database.beginTransaction()
 
-    const trx = await Database.beginTransaction()
-
-    const user = await User.create(data, trx)
-
-    await user.addresses().create(addresses, trx)
-
-    await trx.commit()
+    const user = await User.create(data)
+    // const user = await User.create(data, trx)
+    // await user.addresses().create(addresses, trx)
+    // await trx.commit()
 
     return user
   }
