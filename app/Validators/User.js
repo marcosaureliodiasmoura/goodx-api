b49d6dd733d4445fc0075d8937a80a48bdb5bc4a
MarcosMoura <marcos_moura@live.com>
2019-04-19 22:34:30 -0300

    11. Basic idiom for user validation

--- app/Validators/User.js
+++ app/Validators/User.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Antl = use('Antl')
 class User {
   get validateAll () {
     return true
@@ -11,6 +12,9 @@ class User {
       email: 'required|email|unique:users',
       password: 'required|confirmed'
     }
+  }
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
